d5c9b164bcff28d8822346e4ba64b3c5e4190a4f
krchome <krchome@hotmail.com>
2021-02-17 18:53:05 +1300

    first commit

---
+++ Multilingual Website Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Multilingual_Website_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
